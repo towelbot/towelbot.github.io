50a10b477ba482acfee2b5d21e33fb77f756a5fd
Talbot <talshoe@gmail.com>
2017-12-14 16:15:23 -0800

    Devslopes lecture: Numbers in Javascript

--- Devslopes2/javascript/learn-to-code.js
+++ Devslopes2/javascript/learn-to-code.js
@@ -4,12 +4,24 @@
 //
 // var message = "Hi, my name is " + name + " and I am " + age + " years old.";
 
-var firstName = "John";
-var lastName = "Smith";
-var dateOfBirth = "10-09-82";
-var age = 23;
-var profileImageUrl = "http://SomeDumb.url";
+// var firstName = "John";
+// var lastName = "Smith";
+// var dateOfBirth = "10-09-82";
+// var age = 23;
+// var profileImageUrl = "http://SomeDumb.url";
+//
+// var loginWelcomeMessage = "Welcome " + firstName + ". Happy " + age +"rd birthday!" ;
+//
+// console.log(loginWelcomeMessage);
 
-var loginWelcomeMessage = "Welcome " + firstName + ". Happy " + age +"rd birthday!" ;
+var sum = 10 + 15;
+var sub = 15 - 10;
+var mul = 10 * 3;
+var div = 9 / 3;
+var mod = 10 % 3;
 
-console.log(loginWelcomeMessage);
+var msg = "10 / 3 = 3 with a remainder of " + mod;
+
+var result = 10 * ((5 + 3) - 4);
+
+console.log(sub);
