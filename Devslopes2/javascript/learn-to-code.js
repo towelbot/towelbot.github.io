2be4d8ffc2935507978f31be615d8760622df9ae
Talbot <talshoe@gmail.com>
2017-12-14 16:06:50 -0800

    Devslopes Class: Javascript Variables & Strings

--- Devslopes2/javascript/learn-to-code.js
+++ Devslopes2/javascript/learn-to-code.js
@@ -1,6 +1,15 @@
-var name = "Jack";
+// var name = "Jack";
+// var age = 23;
+// var shootingScore = 45.6;
+//
+// var message = "Hi, my name is " + name + " and I am " + age + " years old.";
+
+var firstName = "John";
+var lastName = "Smith";
+var dateOfBirth = "10-09-82";
 var age = 23;
+var profileImageUrl = "http://SomeDumb.url";
 
-var message = "Hi, my name is " + name + " and I am " + age + " years old.";
+var loginWelcomeMessage = "Welcome " + firstName + ". Happy " + age +"rd birthday!" ;
 
-console.log(message);
+console.log(loginWelcomeMessage);
