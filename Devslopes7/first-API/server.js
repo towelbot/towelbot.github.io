0671bce317b39ce37b206ea5857dae35ed6537f3
Talbot <towel.bot@gmail.com>
2018-03-19 10:30:55 -0700

    Devslopes7 update
    Changes to server.js and the package for class. re: HTTP requests.

--- Devslopes7/first-API/server.js
+++ Devslopes7/first-API/server.js
@@ -1,13 +1,72 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function(request, response) {
-  response.send('My First API!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = [
+  {
+    "id":"23222a",
+    "text":"Eggs"
+  },
+  {
+    "id":"23222b",
+    "text":"Milk"
+  },
+  {
+    "id":"23222c",
+    "text":"Bacon"
+  },
+  {
+    "id":"23222d",
+    "text":"Frogs Legs"
+  }
+];
+
+app.get('/ingredients', function(request, response) {
+  response.send(ingredients);
 });
 
-app.get('/funions', function(req, res) {
-  res.send('Something something funions mr t');
+app.post('/ingredients', function(request, response) {
+  var ingredient = request.body;
+  if (!ingredient || ingredient.text === "") {
+    response.status(500).send({error: "Your ingredient must have text"});
+  } else {
+    ingredients.push(ingredient);
+    response.status(200).send(ingredients);
+  }
 });
+
+app.put('/ingredients/:ingredientId', function(request, response) {
+
+  var ingredientId = request.params.ingredientId;
+  var newText = request.body.text;
+
+  if (!newText || newText === "") {
+    response.status(500).send({error:"You must provide key ingredient text"})
+  } else {
+    var objectFound = false;
+    for (var x = 0; x < ingredients.length; x++) {
+      var ing = ingredients[x];
+
+      if (ing.id === request.params.ingredientId) {
+        ingredients[x].text = newText;
+        objectFound = true;
+        break;
+      }
+    }
+    if (!objectFound) {
+      response.status(500).send({error:"Ingredient ID not found"});
+    } else {
+      response.send(ingredients);
+    }
+  }
+});
+
+// app.get('/funions', function(req, res) {
+//   res.send('Something something funions mr t');
+// });
 
 app.listen(3000, function() {
   console.log("First API running on port 3000!");
